3b9dfaf396b9b6d4e76b06c6091917b3cbd52970
Xu-Dong Yu <yuxd@stu.pku.edu.cn>
2026-04-28 16:59:27 +0800

    make autogen fail fast

--- module/autogen.sh
+++ module/autogen.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+set -e
+
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-(cd $srcdir; aclocal -I ${OFFLINE_MAIN}/share;\
-libtoolize --force; automake -a --add-missing; autoconf)
+(cd "$srcdir"; \
+aclocal -I "${OFFLINE_MAIN}/share"; \
+libtoolize --force --copy; \
+automake -a --add-missing --copy; \
+autoconf)
 
-$srcdir/configure "$@"
+"$srcdir/configure" "$@"
