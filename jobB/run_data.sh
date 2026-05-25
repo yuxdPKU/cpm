4af48091a77ba4cfb712af5d589261031e6caf66
xyu3 <xyu3@ssh.sdcc.bnl.gov>
2026-05-25 10:49:48 -0400

    updates

---
+++ jobB/run_data.sh
@@ -0,0 +1,9 @@
+./run_cpm_b_chain.sh \
+  --input run79516list.txt \
+  --input-is-list \
+  --out-dir output/run79516 \
+  --prefix run79516 \
+  --b2-containers \
+  --b2-unweighted \
+  --no-combined-output \
+  --no-keep-intermediates
---
+++ jobB/run_sim.sh
@@ -0,0 +1,9 @@
+./run_cpm_b_chain.sh \
+  --input simlist.txt \
+  --input-is-list \
+  --out-dir output/sim \
+  --prefix sim \
+  --b2-containers \
+  --b2-unweighted \
+  --no-combined-output \
+  --no-keep-intermediates
